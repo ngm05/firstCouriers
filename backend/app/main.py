7786b609270209058a4f54ab383932bbce347fec
Natalia Michel Lemuz <z5430065@unsw.edu.au>
2026-07-08 17:32:07 +1000

    debugging

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ from backend.app.services.db import origin
 app = FastAPI(title="First")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origin], 
+    allow_origins=["*"], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
